95645d3694f281aebf8989596b97a25423fa3ba0
Şükrü Erdem Gök <62439670+SukruGokk@users.noreply.github.com>
2022-07-02 14:48:50 +0300

    Update Crypto-Converter.py

--- Crypto-Converter.py
+++ Crypto-Converter.py
@@ -1,19 +1,11 @@
-# @date: 19.04.2021
 # @author: Şükrü Erdem Gök
 # @version: Python 3.8
-# @os: Windows 10
-# @github: https://github.com/SukruGokk
 
-# Crypto Converter
-
-# Lib
 from requests import Session
 from json import loads
 
-# Coinmarketcap api url
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-# Add headers(specifiy app type and add api key)
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': YOUR_COINMARKETCAP_API_KEY,
@@ -22,26 +14,24 @@ headers = {
 session = Session()
 session.headers.update(headers)
 
-# Get inputs
 inputType = input('Input Type: ').upper()
 amount = input('Amount: ')
 outputType = input('Output Type: ').upper()
 
-# Parameters about "what we want"
 parameters = {
       'start':'1',
       'limit':'500',
       'convert': outputType
     }
 
-response = session.get(url, params=parameters)# req
-data = loads(response.text)# json
+response = session.get(url, params=parameters)
+data = loads(response.text)
 
-for crypto in data['data']:# find target type
+for crypto in data['data']:
     if crypto['symbol'] == inputType:
         result = round(crypto['quote'][outputType]['price'], 5) *float(amount)
 
-        if result == 0:# If output is smaller than 1, dont round
+        if result == 0:
             result = crypto['quote'][outputType]['price'] * float(amount)
 
         inputName = crypto['name']
